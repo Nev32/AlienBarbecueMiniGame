aadd1bd7e46e9898ebd069bb3ce91f69aaa1414c
NevileRuas <nevilers32@gmail.com>
2023-04-10 22:17:38 -0300

    Update_230410
    Main menu Implementation
    Pause Menu implementation
    Options menu implementation

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MenuController : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public GameObject pauseMenu;
+    static bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicvolume", volume);
+    }
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxvolume", volume);
+    }
+}
